81aa91af6cc486ab1f2896f9361b7638689bd25f
Jin Zheng <jin.zheng@shopee.com>
2026-01-21 15:35:20 +0800

    add langchain fake_chat_models demo.

--- langchain-demo/examples/langchain_base.py
+++ langchain-demo/examples/langchain_base.py
@@ -1,6 +1,39 @@
+from langchain_core.language_models.fake_chat_models import GenericFakeChatModel
+from langchain_core.messages import AIMessage
+
+
 def langchain_help():
     print("langchain / langgraph base examples.")
 
 
+def test_fake_chat_model_01():
+    model = GenericFakeChatModel(messages=iter(["hello", "world"]))
+    print(model.invoke("any").content)
+    print(model.invoke("any").content)
+
+
+def test_fake_chat_model_02():
+    fake_model = GenericFakeChatModel(
+        messages=iter(
+            [
+                AIMessage(
+                    content="",
+                    tool_calls=[
+                        {
+                            "id": "call_1",
+                            "name": "foo",
+                            "args": {"bar": "baz"},
+                        }
+                    ],
+                ),
+                "final anwser",
+            ]
+        )
+    )
+    print(fake_model.invoke("any"))
+    print(fake_model.invoke("any"))
+
+
 if __name__ == "__main__":
-    pass
+    # test_agent_with_fake_chat_model_01()
+    test_fake_chat_model_02()
--- langchain-demo/examples/py_base.py
+++ langchain-demo/examples/py_base.py
@@ -1,11 +1,14 @@
 from copy import replace  # available: py >= 3.13
 from dataclasses import dataclass
+from typing import Iterator, List
 
 from pydantic import SecretStr
 from rich.console import Console
 
+# common
 
-def call_package_fn():
+
+def test_call_package_fn():
     from langchain_base import langchain_help
 
     langchain_help()
@@ -15,6 +18,52 @@ def test_secret_str():
     console = Console()
     s = SecretStr("hello")
     console.print(f"secret string: [bold red]{s.get_secret_value()}[/bold red]")
+
+
+# demo: typing
+
+
+def test_py_typing_list():
+    def print_list(values: List[str | int]):
+        for val in values:
+            if isinstance(val, str):
+                print("string:", val)
+            elif isinstance(val, int):
+                print("number:", val)
+            else:
+                print("invalid value")
+
+    print_list(
+        [
+            "a",
+            "b",
+            1,
+            "d",
+        ]
+    )
+
+
+def test_py_typing_iter():
+    def print_iter(values: Iterator[str | int]):
+        for val in values:
+            if isinstance(val, str):
+                print("string:", val)
+            elif isinstance(val, int):
+                print("number:", val)
+            else:
+                print("invalid value")
+
+    print_iter(iter(["a", "b", 1, "d"]))
+    print()
+
+    def get_iter(total: int) -> Iterator[str | int]:
+        for i in range(1, total):
+            if i % 2 == 0:
+                yield str(i)
+            else:
+                yield i
+
+    print_iter(get_iter(6))
 
 
 # demo: dataclass
@@ -33,6 +82,10 @@ def test_update_immutable_dataclass():
 
 
 if __name__ == "__main__":
-    call_package_fn()
+    # test_call_package_fn()
     # test_secret_str()
+
+    # test_py_typing_list()
+    test_py_typing_iter()
+
     # test_update_immutable_dataclass()
